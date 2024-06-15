9761b4cf320c6d94fa0656c56be55bb5131aea7f
MitchellZeleny <135643132+MitchellZeleny@users.noreply.github.com>
2024-06-15 10:05:04 -0500

    Update main.cpp
    finish find shoe, then done.

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <array>
 using namespace std;
 
+// Objects inside Shoe vector
 class Shoe
 {
     public:
@@ -12,49 +13,93 @@ class Shoe
     string shoeColor[] = {};
     int shoeYear[] = {};
     string shoeName[] = {};
-    int instr;
-    vector<Shoe> shoeCollection;
-};
+}
 
+// Function to add a shoe to your collection
 class addShoe
 {
+    string addName;
+    string addBrand;
+    string addColor;
+    int addYear
+
     if (instr == 1)
     {
-        cout << "Enter the brand of the shoe in your collection: " << endl;
-        getine(cin, shoeBrand);
-        cout << "Enter the primary color of your " << shoeBrand << " shoe: " << endl;
-        getline(cin, shoeColor);
-        cout << "Enter the year your " << shoeColor << " " << shoeBrand << "'s were made: " << endl;
-        cin >> shoeYear;
-        cout << "Enter the name of your " << shoeYear << " " << shoeColor << " " << shoeBrand << "'s: " << endl;
-        getline(cin, shoeName);
-    };    
-};
+        cout << "Enter the brand of the shoe you want to add to your collection: " << endl;
+        getine(cin, addBrand);
+        shoeBrand.push_back(addBrand);
+        cout << "Enter the primary color of your " << addBrand << " shoe: " << endl;
+        getline(cin, addColor);
+        shoeColor.push_back(addColor);
+        cout << "Enter the year your " << addColor << " " << addBrand << "'s were made: " << endl;
+        cin >> addYear;
+        shoeYear.push_back(addYear);
+        cout << "Enter the name of your " << addYear << " " << addColor << " " << addBrand << "'s: " << endl;
+        getline(cin, addName);
+        shoeName.push_back(addName);
+    }    
+}
 
+// Function to delete shoe from your collection
 class dltShoe
 {
+    string dltName;
+    string dltBrand;
+    string dltColor;
+    int dltYear;
+
     if (instr == 2)
     {
-        string dltName;
-        string dltBrand;
-        string dltColor;
-        int dltYear;
-    };
-};
+        cout << "Enter the brand of the shoe you want to delete from your collection: " << endl;
+        getine(cin, dltBrand);
+        cout << "Enter the primary color of your " << dltBrand << " shoe: " << endl;
+        getline(cin, dltColor);
+        cout << "Enter the year your " << dltColor << " " << dltBrand << "'s were made: " << endl;
+        cin >> dltYear;
+        cout << "Enter the name of your " << dltYear << " " << dltColor << " " << dltBrand << "'s: " << endl;
+        getline(cin, dltName);
+        int size = sizeof(shoeBrand) - 1;
+        for (int i = 0; i < size; i++)
+        {
+            if (shoeBrand[i] == dltBrand && shoeColor[i] == dltColor && shoeYear[i] == dltYear && shoeName[i] == dltName)
+            {
+                shoeBrand.remove(dltBrand);
+                shoeColor.remove(dltColor);
+                shoeYear.remove(dltYear);
+                shoeName.remove(dltName);
+            }
+            else
+            {
+                cout << "This shoe is not in your collection, add it!" << endl;
+            }
+        }
+    }
+}
 
+// Function to see if a shoe is already in your collection
 class findShoe
 {
+    string tempName;
+    string tempBrand;
+    string tempColor;
+    int tempYear;
     if (instr == 3)
     {
-        string tempName;
-        string tempBrand;
-        string tempColor;
-        int tempYear;
-    };
-};
+        cout << "Enter the brand of the shoe you want to know if it is already in your collection or not: " << endl;
+        getine(cin, tempBrand);
+        cout << "Enter the primary color of your " << tempBrand << " shoe: " << endl;
+        getline(cin, tempColor);
+        cout << "Enter the year your " << tempColor << " " << tempBrand << "'s were made: " << endl;
+        cin >> tempYear;
+        cout << "Enter the name of your " << tempYear << " " << tempColor << " " <<tempBrand << "'s: " << endl;
+        getline(cin, tempName);
+    }
+}
 
 int main()
 {
+    vector<Shoe> shoeCollection; 
+    int instr;
     cout << "Enter 1 to add a shoe to your collection." << endl;
     cout << "Eenter 2 to delete a shoe from your collection." << endl;
     cout << "Enter 3 to search for a shoe in your collection." << endl;
