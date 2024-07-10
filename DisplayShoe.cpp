52b51faa2f04efed048c57a61010a0b3fc3c75d6
MitchellZeleny <135643132+MitchellZeleny@users.noreply.github.com>
2024-07-10 11:39:31 -0500

    Create DisplayShoe.cpp

---
+++ DisplayShoe.cpp
@@ -0,0 +1,16 @@
+#include <array>
+#include <string>
+#include <iostream>
+#include <vector>
+#include "ShoeClass.cpp"
+using namespace std;
+
+// Function to display your shoe collection in a neat format
+class DisplayShoe
+{
+public:
+	for (int i = 0; i < shoeBrand.size(); i++)
+	{
+		cout << shoeBrand[i] << "\t" << shoeColor[i] << "\t" << shoeYear[i] << "\t" << shoeName[i] << endl;
+	}
+};
